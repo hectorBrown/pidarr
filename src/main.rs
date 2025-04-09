be8138a2507d0d0790f188d8a4556ffb0aad780c
Hector Brown <hectorjbrown@protonmail.com>
2025-04-09 16:39:56 +1200

    setup settings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,39 @@
+use leptos::mount::mount_to_body;
 use leptos::prelude::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(App);
 }
 
+#[component]
+pub fn App() -> impl IntoView {
+    let radarr_addr = RwSignal::new("127.0.0.1:7878".to_string());
+    let qbit_addr = RwSignal::new("127.0.0.1:8888".to_string());
+
+    view! {
+        <table>
+            <tr>
+                <th><p> Radarr address: </p></th>
+                <th><p>
+                    <input type="text"
+                        bind:value=radarr_addr
+                    />
+                </p></th>
+            </tr>
+            <tr>
+                <th><p> qBittorrent address: </p></th>
+                <th><p>
+                    <input type="text"
+                        bind:value=qbit_addr
+                    />
+                </p></th>
+            </tr>
+        </table>
+        <button on:click=|_| {
+                //TODO: save addresses
+            }
+        >Save</button>
+
+    }
+}
